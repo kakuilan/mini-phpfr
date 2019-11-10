a30ecd233722a7e00664b6fe8e4115f08c421d0a
kakuilan <kakuilan@163.com>
2019-11-10 15:00:22 +0800

    add loadConf/getConf

--- app/Services/AppService.php
+++ app/Services/AppService.php
@@ -4,18 +4,44 @@
  * User: kakuilan
  * Date: 2019/11/10
  * Time: 14:23
- * Desc:
+ * Desc: 应用服务类
  */
 
 namespace App\Services;
 
+use Symfony\Component\Yaml\Yaml;
+
 class AppService extends ServiceBase {
+
+    private static $conf;
+
+
+    /**
+     * 载入配置
+     */
+    private static function loadConf() {
+        if(is_null(self::$conf)) {
+            self::$conf = Yaml::parseFile(CONFDIR . 'conf.yml');
+        }
+    }
+
+
+    /**
+     * 获取配置
+     * @param string|null $key 配置键,为空则取全部配置
+     * @return mixed|null
+     */
+    public static function getConf(string $key=null) {
+        $res = $key ? (self::$conf[$key] ?? null) : self::$conf;
+        return $res;
+    }
 
 
     /**
      * 应用初始化
      */
     public static function init() {
+        self::loadConf();
 
     }
 
